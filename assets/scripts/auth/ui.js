8681a36a9162464c6dc0c435ba8c6dbbf8b37c39
gundyn <nathan.gundy@gmail.com>
2020-10-19 15:58:31 -0500

    game logic complete need to reset the board for a new game from a tie or when you just want a new game

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -94,9 +94,30 @@ const onSquareClickSuccess = (response) => {
   function determineWinner () {
     console.log('game board', store.event.target)
     console.log('the array of cells', store.game.cells)
-    console.log('the zero position in the array', store.game.cells[0])
-    console.log('the 1 position in the array', store.game.cells[1])
-    console.log('the 2 postion in the array', store.game.cells[2])
+
+    if (store.game.cells[0] === 'x' && store.game.cells[1] === 'x' && store.game.cells[2] === 'x') {
+      $('#message').text('Top row wins!')
+    } else if (store.game.cells[3] === 'x' && store.game.cells[4] === 'x' && store.game.cells[5] === 'x') {
+      $('#message').text('Middle row wins!')
+    } else if (store.game.cells[6] === 'x' && store.game.cells[7] === 'x' && store.game.cells[8] === 'x') {
+      $('#message').text('Bottom row wins!')
+    } else if (store.game.cells[0] === 'x' && store.game.cells[4] === 'x' && store.game.cells[8] === 'x') {
+      $('#message').text('Diagonal row wins!')
+    } else if (store.game.cells[2] === 'x' && store.game.cells[4] === 'x' && store.game.cells[6] === 'x') {
+      $('#message').text('Diagonal row wins!')
+    } else if (store.game.cells[0] === 'o' && store.game.cells[1] === 'o' && store.game.cells[2] === 'o') {
+      $('#message').text('Top row wins!')
+    } else if (store.game.cells[3] === 'o' && store.game.cells[4] === 'o' && store.game.cells[5] === 'o') {
+      $('#message').text('Middle row wins!')
+    } else if (store.game.cells[6] === 'o' && store.game.cells[7] === 'o' && store.game.cells[8] === 'o') {
+      $('#message').text('Bottom row wins!')
+    } else if (store.game.cells[0] === 'o' && store.game.cells[4] === 'o' && store.game.cells[8] === 'o') {
+      $('#message').text('Diagonal row wins!')
+    } else if (store.game.cells[2] === 'o' && store.game.cells[4] === 'o' && store.game.cells[6] === 'o') {
+      $('#message').text('Diagonal row wins!')
+    } else if (store.game.cells[0] !== '' && store.game.cells[1] !== '' && store.game.cells[2] !== '' && store.game.cells[3] !== '' && store.game.cells[4] !== '' && store.game.cells[5] !== '' && store.game.cells[6] !== '' && store.game.cells[7] !== '' && store.game.cells[8] !== '') {
+      $('#message').text('Game is a tie!')
+    }
   }
 }
 
